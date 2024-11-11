40f09884b8e27c0060333da3dea81efd1cfcef38
John the Fox <joao.chivite@gmail.com>
2024-11-11 18:03:23 -0300

    tela de login (logo incorreto)

--- app/src/main/java/com/example/cardapio/MainActivity.kt
+++ app/src/main/java/com/example/cardapio/MainActivity.kt
@@ -1,20 +1,50 @@
 package com.example.cardapio
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cardapio.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Inicializa os componentes da interface usando o ViewBinding
+        val emailField = binding.email
+        val passwordField = binding.password
+        val loginBtn = binding.login
+        val registerBtn = binding.register
+        val passwordRecover = binding.passwordRecover
+
+        loginBtn.setOnClickListener{
+            val auth = FirebaseAuth.getInstance()
+            val email = emailField.text.toString()
+            val password = passwordField.text.toString()
+            auth.signInWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    val user = auth.currentUser
+                    Toast.makeText(this, "Login efetuado com sucesso!", Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this, "Erro no login: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        registerBtn.setOnClickListener{
+
+        }
+
+        passwordRecover.setOnClickListener{
+
         }
     }
 }
